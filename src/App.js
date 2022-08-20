c44a32676d32d0cab02e2b683ce70b7c60fc5428
sameh <sameh080081@gmail.com>
2022-08-20 11:32:23 +0400

    add book components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import NavBar from "./components/Navbar";
+import Catagories from "./pages/Catagorypage";
+import Boookpage from "./pages/Bookpage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Boookpage />} />
+        <Route path="/catagories" element={<Catagories />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+);
 
 export default App;
